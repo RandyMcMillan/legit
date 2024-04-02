abe35b0c5d1c4c521e8e343cb0e93be6248cac35
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2024-04-02 13:19:58 -0400

    cargo.mk:cargo-i:cargo-c:cargo-br

--- cargo.mk
+++ cargo.mk
@@ -1,15 +1,19 @@
 cargo-help:### 	
 	@awk 'BEGIN {FS = ":.*?###"} /^[a-zA-Z_-]+:.*?###/ {printf "\033[36m%-15s\033[0m %s\n", $$1, $$2}' $(MAKEFILE_LIST)
+cargo-b:cargo-build
 cargo-build:###	cargo-build
 	@. $(HOME)/.cargo/env
 	@cargo b
+cargo-i:cargo-install
 cargo-install:###	cargo-install
 	@. $(HOME)/.cargo/env
 	@cargo install --path $(PWD)
 	@echo "export PATH=$(CARGO_PATH)/bin:$(PATH)"
+cargo-br:cargo-build-release
 cargo-build-release:###	cargo-build-release
 	@. $(HOME)/.cargo/env
 	@cargo b --release
+cargo-c:cargo-check
 cargo-check:###	cargo-check
 	@. $(HOME)/.cargo/env
 	@cargo c
